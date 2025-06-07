123482c9689072133ef57fc593db5dbac5435b3b
朝阳 <chaoyang@zhaoyangdeMacBook-Air.local>
2025-06-08 00:44:48 +0800

    feat: add pages directory with index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>🎉 Hello, Next.js!</h1>
+      <p>你的站点已成功配置 pages/index.js，编辑这个文件开始吧。</p>
+    </div>
+  )
+}
