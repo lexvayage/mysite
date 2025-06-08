30b4ab1480d63168df6c1c8d3e7aa51eab10afdd
朝阳 <chaoyang@zhaoyangdeMacBook-Air.local>
2025-06-08 16:55:05 +0800

    feat: customize homepage content

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
-      <h1>🎉 Hello, Next.js!</h1>
-      <p>你的站点已成功配置 pages/index.js，编辑这个文件开始吧。</p>
+      <h1>🎉 Hello, 欢迎来到朱朝阳的网站
+      <p>这里是我的个人博客。</p>
     </div>
   )
 }
