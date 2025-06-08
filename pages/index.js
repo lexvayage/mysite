4796d178304e30a94fd1ae589aae9726820d862a
朝阳 <chaoyang@zhaoyangdeMacBook-Air.local>
2025-06-08 17:08:02 +0800

    fix: 更新首页文本

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
-      <h1>🎉 Hello, 欢迎来到朱朝阳的网站
+      <h1>🎉 Hello, 欢迎来到朱朝阳的网站</h1>
       <p>这里是我的个人博客。</p>
     </div>
   )
