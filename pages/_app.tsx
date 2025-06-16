d2e05b8044019b4e3e612a85e081e594708e8a5b
朝阳 <chaoyang@zhaoyangdeMacBook-Air.local>
2025-06-16 15:25:04 +0800

    chore: 配置 GA4 Measurement ID

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,29 @@
 // pages/_app.tsx
-import '../styles/globals.css'    // ← 确保路径和你的 globals.css 对应
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
+        strategy="afterInteractive"
+      />
+      <Script id="ga-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-XXXXXXXXXX', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+export default MyApp
